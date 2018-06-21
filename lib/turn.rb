1c198e3886cc58d0af38f316d68d6923d09685fb
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-06-21 14:19:34 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,4 +1,16 @@
 def display_board(board)
+  filled_board = []
+  board = board.each_slice(3).to_a
+  board.each do |a|
+    filled_board << a.join(" | ")
+  end
+  for row in (0..filled_board.length-1)
+    if row > 0
+      print "-----------\n"
+    end
+    print " " + filled_board[row] + " \n"
+  end
+  nil
 end
 
 def input_to_index(input)
@@ -12,4 +24,5 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
+  gets.strip
 end
